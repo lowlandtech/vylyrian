7186a6485b60aba62c827e8442cb3121b317ce72
wendellmva <wendellmva@lowlandtech.com>
2025-03-27 12:37:10 +0100

    feat(#2): enhance MainLayout with dynamic drawer and responsive design
    Updated `MainLayout.razor` to improve layout and user experience by adding a dynamic class to the drawer toggle button and a new `MudText` component displaying "Vylyr". CSS has been modified for smoother icon transitions.
    
    Introduced `MainLayout.razor.cs` to encapsulate component logic, including `AppVm` injection, drawer icon state management, and responsive behavior through the `HandleBreakpointChanged` method.

---
+++ src/lowlandtech.vylyr/Layout/MainLayout.razor.cs
@@ -0,0 +1,28 @@
+﻿namespace LowlandTech.Vylyr.Layout;
+
+public partial class MainLayout : IDisposable
+{
+    [Inject] private AppVm App { get; set; } = default!;
+
+    private string DrawerIcon => App.DrawerOpen ? Icons.Material.Filled.Close : Icons.Material.Filled.Menu;
+
+    protected override void OnInitialized()
+    {
+        App.OnChange += () => InvokeAsync(StateHasChanged);
+    }
+
+    public void Dispose()
+    {
+        App.OnChange -= () => InvokeAsync(StateHasChanged);
+    }
+
+    public Task HandleBreakpointChanged(Breakpoint breakpoint)
+    {
+        if (App.IsNativeMobile) return Task.CompletedTask;
+
+        App.IsMobile = breakpoint <= Breakpoint.Sm;
+        App.DrawerOpen = !App.IsMobile;
+        StateHasChanged();
+        return Task.CompletedTask;
+    }
+}
